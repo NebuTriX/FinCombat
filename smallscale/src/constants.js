36f4f69534d2c5b07670c0f651ddd37552549028
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2024-05-17 20:45:09 +0530

    :zap: Smallscale contains the complete ver. to test out how to create a game
    Mazza nahin aaya hence change of plan

---
+++ smallscale/src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Welcome to FinCombat! This is your trusty computer, where you'll manage your finances and learn about budgeting, saving, investing, and more.
+    Let's dive into the world of financial education and embark on this journey together!
+    Here's a <a href="https://github.com/NebulaTris/FinCombat" target="_blank">link</a> to learn more!`,
+  "cs-degree": `This is my Finance Degree. I proudly display it as it represents my understanding of financial concepts and principles. It has equipped me with the knowledge needed to navigate the world of personal finance.`,
+  "sofa-table": `This is my cozy sofa, where I like to unwind and explore financial resources. 
+  You can also find financial education videos on various platforms to enhance your financial knowledge.`,
+  tv: `That's my TV. Lately, I've been watching financial experts online, including: <a href="https://www.youtube.com/channel/UCIALMKvObZNtJ6AmdCLP7Lg" target="_blank">Bloomberg Markets And Finance</a>, 
+  <a href="https://www.youtube.com/@thefinancialdiet/videos" target="_blank">The Financial Diet</a>, <a href="https://www.youtube.com/@YourMoneyYourWealth/featured" target="_blank">Your Money, Your Wealth</a>, 
+  and <a href="https://www.youtube.com/@JackCorsellis" target="_blank">Jack Corsellis</a>!`,
+  bed: `This is where I recharge and reflect on my financial goals. Sometimes, my best financial ideas come to me when I'm about to fall asleep. 
+  It's essential to jot them down to ensure I don't forget them.`,
+  resume: `This is a financial cheat sheet for reference. <a href="https://github.com/NebulaTris/FinCombat/blob/main/FinancialCheatsheet.pdf" target="_blank">Check it out?</a>`,
+  projects: `About FinCombat: It's built using JavaScript and Kaboom.js library, designed specifically for creating interactive financial experiences.
+  Text is displayed using HTML/CSS, providing an engaging and educational environment. Learn more about financial literacy by exploring tutorials and resources online.`,
+  library: `My shelves are filled with financial books, including one in Hindi (I'm multilingual). Although I haven't read them all, they serve as valuable resources for expanding my financial knowledge.
+  Do you also have a collection of financial books waiting to be read?`,
+  exit: `If you're ready to exit FinCombat, simply close the tab.`,
+};
